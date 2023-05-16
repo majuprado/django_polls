c3b73c52447f1fa4b86b72c762e5f77c8950844e
Kauan machado <kauan.barbosa@aluno.ifsp.edu.br>
2023-05-16 10:30:24 -0300

    a

--- my_site/urls.py
+++ my_site/urls.py
@@ -16,7 +16,7 @@ Including another URLconf
 """
 from xml.etree.ElementInclude import include
 from django.contrib import admin
-from django.urls import path
+from django.urls import include, path
 
 urlpatterns = [
     path("polls/", include('polls.urls')),
