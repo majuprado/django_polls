bf3a572a0b14c5512725504aff56378eb2bd9fd9
Kauan machado <kauan.barbosa@aluno.ifsp.edu.br>
2023-05-16 10:15:58 -0300

    i

--- polls/views.py
+++ polls/views.py
@@ -3,5 +3,12 @@ from django.http import HttpResponse
 
 # Create your views here.
 
+from django.http import HttpResponse
+
+# Create your views here
+
 def index(request):
     return HttpResponse('Olá... seja bem vindo à enquete')
+
+def sobre(request):
+    return HttpResponse('Olá este é um app de enquete')
