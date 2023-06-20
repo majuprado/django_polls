f187bfeae8315cf3e42b6ee9ba138b7ee06f0fa5
Kauan machado <kauan.barbosa@aluno.ifsp.edu.br>
2023-06-20 10:53:34 -0300

    Inclui página com as ultimas perguntas

--- polls/urls.py
+++ polls/urls.py
@@ -6,4 +6,5 @@ urlpatterns = [
     path('', views.index, name='index'),
     path('sobre/', views.sobre, name='sobre'),
     path('pergunta/<int:question_id>', views.exibe_questao, name='exibe_questao'),
+    path('perguntas', views.ultimas_perguntas, name='ultimas_perguntas'),
 ]
