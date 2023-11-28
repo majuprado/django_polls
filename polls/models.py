c36f804553a1cb427825359ac1360f5e1fc8abfa
Maju <m.prado@aluno.ifsp.edu.br>
2023-11-28 11:14:26 -0300

    Votação única por usuário

--- polls/models.py
+++ polls/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.core.exceptions import ValidationError
 
 from django.contrib.auth import get_user_model
 User = get_user_model()
@@ -9,7 +10,7 @@ class Question(models.Model):
     question_text = models.CharField(max_length=200)
     pub_date = models.DateTimeField("date published")
     author = models.ForeignKey(
-        User, 
+        User,
         editable=False,
         on_delete=models.DO_NOTHING,
         null=True
@@ -26,3 +27,17 @@ class Choice(models.Model):
 
     def __str__(self):
         return self.choice_text
+
+    def save(self, user=None, *args, **kwargs):
+        if self.id is not None and user is not None:
+            question_user = QuestionUser.objects.filter(user=user, question=self.question).count()
+            if question_user > 0:
+                raise ValidationError('Não é permitido votar mais de uma vez')
+            question_user = QuestionUser.objects.create(user=user, question=self.question)
+            question_user.save()
+        super().save(*args, **kwargs)
+
+
+class QuestionUser(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    question = models.ForeignKey(Question, on_delete=models.CASCADE)
--- polls/views.py
+++ polls/views.py
@@ -15,6 +15,10 @@ from django.contrib import messages
 from django.contrib.auth.decorators import login_required
 from django.contrib.auth.mixins import LoginRequiredMixin
 from django.db.models import Sum
+from django.core.exceptions import ValidationError
+
+from django.contrib.auth import get_user_model
+User = get_user_model()
 
 
 # Create your views here
@@ -34,16 +38,13 @@ def exibe_questao(request, question_id):
 
     if questao is not None:
         return HttpResponse(questao.question_text)
-    
+
     return HttpResponse('Não existe questao a exibir')
 
 def ultimas_perguntas(request):
     latest_question_list = Question.objects.order_by('-pub_date')[:5]
     context = {'latest_question_list': latest_question_list}
     return render(request, 'polls/perguntas_recentes.html', context)
-
-def vote(request, question_id):
-    return HttpResponse(f"Você vai votar na pergunta ")
 
 
 
@@ -79,7 +80,7 @@ class QuestionUpdateView(UpdateView):
         context['question_choices'] = choices
 
         return context
-        
+
     def form_valid(self, request, *args, **kwargs):
         messages.success(self.request, self.success_message)
         return super(QuestionUpdateView, self).form_valid(request, *args, **kwargs)
@@ -136,7 +137,7 @@ class ChoiceCreateView(CreateView):
         context ['form_title'] = f'Alternatva para: {question.question_text}'
 
         return context
-    
+
     def form_valid(self, form):
         form.instance.question = self.question
         messages.success(self.request, self.success_message)
@@ -145,53 +146,58 @@ class ChoiceCreateView(CreateView):
     def get_success_url(self, *args, **kwargs):
         question_id = self.kwargs.get('pk')
         return reverse_lazy("poll_edit", kwargs = {"pk" : question_id})
-    
+
 class ChoiceUpdateView(UpdateView):
-    model = Choice 
-    template_name = 'polls/choice_form.html' 
-    fields = ('choice_text', )  
+    model = Choice
+    template_name = 'polls/choice_form.html'
+    fields = ('choice_text', )
     success_message = ("Alternativa atualizada com sucesso!")
 
-    def get_context_data(self, **kwargs): 
+    def get_context_data(self, **kwargs):
         question = get_object_or_404 (Question, pk = self.object.question.id)
-        context = super (ChoiceUpdateView, self).get_context_data(**kwargs) 
-        context['form_title'] = 'Editando alternativa' 
-        return context 
-    
-    def form_valid(self, request, *args, **kwargs): 
-        messages.success(self.request, self.success_message) 
+        context = super (ChoiceUpdateView, self).get_context_data(**kwargs)
+        context['form_title'] = 'Editando alternativa'
+        return context
+
+    def form_valid(self, request, *args, **kwargs):
+        messages.success(self.request, self.success_message)
         return super(ChoiceUpdateView, self).form_valid(request, *args , **kwargs)
-    
-    def get_success_url(self, *args, **kwargs): 
-        question_id = self.object.question.id 
+
+    def get_success_url(self, *args, **kwargs):
+        question_id = self.object.question.id
         return reverse_lazy('poll_edit', kwargs={'pk': question_id})
 
-class ChoiceDeleteView(LoginRequiredMixin, DeleteView): 
-     model = Choice 
-     template_name = 'polls/choice_confirm_delete_form.html'
-     success_message = 'Alternativa excluída com sucesso!' 
-     
-     def form_valid(self, request, *args, **kwargs):
-         messages.success(self.request, self.success_message) 
-         return super (ChoiceDeleteView, self).form_valid(request, *args, **kwargs)
-     
-     def get_success_url(self, *args, **kwargs): 
-        question_id = self.object.question.id 
-        return reverse_lazy('poll_edit', kwargs={'pk': question_id}) 
+class ChoiceDeleteView(LoginRequiredMixin, DeleteView):
+    model = Choice
+    template_name = 'polls/choice_confirm_delete_form.html'
+    success_message = 'Alternativa excluída com sucesso!' 
 
+    def form_valid(self, request, *args, **kwargs):
+        messages.success(self.request, self.success_message)
+        return super (ChoiceDeleteView, self).form_valid(request, *args, **kwargs)
+
+    def get_success_url(self, *args, **kwargs):
+        question_id = self.object.question.id
+        return reverse_lazy('poll_edit', kwargs={'pk': question_id})
+
+@login_required
 def vote(request, question_id):
     question = get_object_or_404(Question, pk=question_id)
     if request.method == 'POST':
         try:
             selected_choice = question.choice_set.get(pk=request.POST["choice"])
+            selected_choice.votes += 1
+            session_user = get_object_or_404(User, id=request.user.id)
+            selected_choice.save(user=session_user)
         except (KeyError, Choice.DoesNotExist):
             messages.error(request, 'Selecione uma alternativa para votar!')
+
+        except (ValidationError) as error:
+            messages.error(request, error.message)
         else:
-            selected_choice.votes += 1
-            selected_choice.save()
             messages.success(request, 'Seu voto foi registrado com sucesso!')
             return redirect(reverse_lazy("poll_results", args=(question.id,)))
-        
+
     context = {'question': question}
     return render(request, 'polls/question_detail.html', context)
 
